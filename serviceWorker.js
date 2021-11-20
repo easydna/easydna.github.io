bbea1a598449c28eaf5c06d170f2eaa0da7106be
easydna <94758570+easydna@users.noreply.github.com>
2021-11-20 15:09:10 -0300

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,38 @@
+const staticEasyDNA = "easy-DNA-v1"
+
+const assets = [
+
+  "./",
+  "./index.html",
+  "./scripts/axios.js",
+  "./scripts/dexie.js",
+  "./scripts/vue.js",
+  "./scripts/uuidv4.js",
+  "./scripts/email.js",
+
+  "./icons/android-chrome-192x192.png",
+  "./icons/android-chrome-512x512.png",
+  "./icons/apple-touch-icon.png",
+  "./icons/favicon-32x32.png",
+
+  "./listas/equipamento.js",
+  "./listas/equipe.js",
+  "./listas/situacao.js",
+
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticEasyDNA).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+})
