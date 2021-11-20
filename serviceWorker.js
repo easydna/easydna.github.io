61cbf0e093be5b82dc87823bbd799ed091271d99
easydna <94758570+easydna@users.noreply.github.com>
2021-11-20 15:12:21 -0300

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,23 +2,22 @@ const staticEasyDNA = "easy-DNA-v1"
 
 const assets = [
 
-  "./",
-  "./index.html",
-  "./scripts/axios.js",
-  "./scripts/dexie.js",
-  "./scripts/vue.js",
-  "./scripts/uuidv4.js",
-  "./scripts/email.js",
+  "/",
+  "/index.html",
+  "/scripts/axios.js",
+  "/scripts/dexie.js",
+  "/scripts/vue.js",
+  "/scripts/uuidv4.js",
+  "/scripts/email.js",
 
-  "./icons/android-chrome-192x192.png",
-  "./icons/android-chrome-512x512.png",
-  "./icons/apple-touch-icon.png",
-  "./icons/favicon-32x32.png",
+  "/icons/android-chrome-192x192.png",
+  "/icons/android-chrome-512x512.png",
+  "/icons/apple-touch-icon.png",
+  "/icons/favicon-32x32.png",
 
-  "./listas/equipamento.js",
-  "./listas/equipe.js",
-  "./listas/situacao.js",
-
+  "/listas/equipamento.js",
+  "/listas/equipe.js",
+  "/listas/situacao.js",
 ]
 
 self.addEventListener("install", installEvent => {
